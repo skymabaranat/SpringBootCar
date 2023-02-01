2ca501bf271bc8a4485aca1fd4ff4a2e5adb6bd1
TrevinKM <t.mabarana@lancaster.ac.uk>
2023-02-01 11:17:23 +0000

    First commit

---
+++ src/main/java/com/project/springbootcar/SpringBootCarApplication.java
@@ -0,0 +1,13 @@
+package com.project.springbootcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/springbootcar/SpringBootCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springbootcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
